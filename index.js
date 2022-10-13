d252c8bbe3c887bb2c4f5e5914744580587853fe
airtatsumaki <naz1mb20@gmail.com>
2022-10-13 14:47:21 +0100

    boiler plate

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 import express from 'express';
+const app = express();
+
+app.get("/", (req,res) => {
+  res.send("Hello there");
+});
+
+app.listen(process.env.PORT || 3000, () => console.log("Server is running"));
