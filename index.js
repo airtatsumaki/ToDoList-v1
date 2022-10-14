7c3ad96010947cf6e9c70b053db340d2350cf369
tatsu <naz1mb20@gmail.com>
2022-10-14 23:30:01 +0100

    add EJS and index.ejs

--- index.js
+++ index.js
@@ -7,18 +7,27 @@ const app = express();
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
 
+app.set('view engine', 'ejs');
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.get("/", (req,res) => {
   const today = new Date();
-  if ([0,6].indexOf(today.getDay()) > -1 ){
-    res.send("Yay it's the weekend!");
-  }
-  else{
-    // res.send("Sorry its a normal working day, Get back to work!");
-    res.sendFile(__dirname + "/index.html");
-  }
+  const dayObj = {};
+  const daysOfTheWeek =  ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  dayObj["today"] = daysOfTheWeek[today.getDay()];
+  // if ([0,6].indexOf(today.getDay()) > -1 ){
+  //   //res.send("Yay it's the weekend!");
+  //   dayObj["message"] = "Yay it's the weekend!";
+  // }
+  // else{
+  //   // res.send("Sorry its a normal working day, Get back to work!");
+  //   // res.sendFile(__dirname + "/index.html");
+  //   dayObj["message"] = "Sorry its a normal working day, Get back to work!";
+  //   //res.render("pages/index");
+  // }
+  res.render("pages/index", {theDay: dayObj});
 });
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running"));
