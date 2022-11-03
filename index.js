f0baba9a44deded6f77ecf0686f6a1a584920fa1
tatsu <naz1mb20@gmail.com>
2022-11-03 15:56:06 +0000

    templating, partials, and include

--- index.js
+++ index.js
@@ -14,19 +14,37 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 let items = ["Do stuff","eat stuff","break stuff"];
+let workList = [];
 
 app.get("/", (req,res) => {
   const myDate = new Date();
   const options = { weekday: 'long', month: 'long', day: 'numeric' };
   const today = myDate.toLocaleDateString('en-GB', options);
-  res.render("pages/index", {theDate: today, todolist: items});
+  res.render("pages/index", {title: today, todolist: items, action: "/"});
+});
+
+app.get("/work", (req,res) => {
+  res.render("pages/index", {title: "Work List", todolist: workList, action: "/work"});
+});
+
+app.get("/about", (req,res) => {
+  res.render("pages/about");
 });
 
 app.post("/", (req, res) => {
-  if (req.body.task.trim() !== ""){
-    items.push(req.body.task);
+  console.log(req.body);
+  if(req.body.button == "Work List"){
+    if (req.body.task.trim() !== ""){
+      workList.push(req.body.task);
+    }
+    res.redirect("/work");
   }
-  res.redirect("/");
+  else{
+    if (req.body.task.trim() !== ""){
+      items.push(req.body.task);
+    }
+    res.redirect("/");
+  }
 });
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running on port 3000"));
